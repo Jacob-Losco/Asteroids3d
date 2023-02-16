550b69aa409110ac13e10969b6680dcf8fa20048
David Richman <david.c.richman@gmail.com>
2023-02-16 09:46:18 -0500

    Better Despawn Mechanics

--- AsteroidsThreeDee/Assets/Scripts/AsteroidMove.cs
+++ AsteroidsThreeDee/Assets/Scripts/AsteroidMove.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class AsteroidMove : MonoBehaviour
 {
     public ManageAsteroids manager;
-    public float despawnDistance = 100;
     public float speed = 5;
     public Vector3 dir;
     public int size = 1;
     public bool locked = false; //used for testing only!
+    private bool despawning = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,33 +21,42 @@ public class AsteroidMove : MonoBehaviour
         }
     }
 
-    void DriftAway()
+    void CheckDespawn()
     {
-        Vector3 relpos = manager.transform.position - this.transform.position;
-        if (relpos.magnitude > despawnDistance)
+        Vector3 relpos = this.transform.position - manager.transform.position;
+        float angle = Mathf.Abs(Vector3.Angle(relpos, manager.transform.forward));
+        float despawnRadius = manager.maxSpawnRadius;
+        if (angle > 110) despawnRadius /= manager.rearCulling;
+        if (relpos.magnitude > despawnRadius)
         {
-            Destroy(this.gameObject);
+            despawning = true;
+            StartCoroutine(Despawn());
         }
+        else despawning = false;
+    }
+    IEnumerator Despawn()
+    {
+        yield return new WaitForSeconds(Random.Range(0,5)); //delay despawn if player returns 
+        if (despawning) Destroy(this.gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        DriftAway();
+        CheckDespawn();
         this.transform.position += speed * Time.deltaTime * dir;
     }
-
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Collided");
 
-        GameObject obj = collision.gameObject;
-        Vector3 diff = (collision.transform.position - this.transform.position).normalized;
+        GameObject obj = other.gameObject;
+        Vector3 diff = (this.transform.position-other.transform.position).normalized;
         if (obj.tag == "Asteroid")
         {
             AsteroidMove src = obj.GetComponent<AsteroidMove>();
-            dir += diff * 0.5f;
-            src.dir -= diff * 0.5f;
+            dir += diff;
+            src.dir -= diff;
             
         }
     }
--- AsteroidsThreeDee/Assets/Scripts/ManageAsteroids.cs
+++ AsteroidsThreeDee/Assets/Scripts/ManageAsteroids.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class ManageAsteroids : MonoBehaviour
 {
     // Start is called before the first frame update
-    public int spawnNum = 3;
+    private int spawnNum = 10;
+    public float spawnDensity = 0.5f;
     public float spawnRate = 2;
+    private float _spawnRate = 1;
     public float minSpawnRadius = 10;
     public float maxSpawnRadius = 50;
+    public float rearCulling = 100;
     //public int level = 1;
     //public float levelRate = 5; // level changes once every levelRate seconds
     public List<GameObject> asteroids;
@@ -21,21 +24,15 @@ public class ManageAsteroids : MonoBehaviour
         //player = Instantiate(playerPrefab);
         //player.GetComponent<SpaceshipMove>().manager = this;
         //player.transform.position = Vector2.zero;
+       
         if (asteroids.Count == 0)
             return;
         lastSpawn = Time.time - spawnRate;
         
     }
-
-    private IEnumerator Despawn(GameObject obj)
-    {
-        yield return new WaitForSeconds(Random.Range(1f, 4f));
-        Destroy(obj);
-    }
-
     private IEnumerator SpawnAsteroid()
     {
-        yield return new WaitForSeconds(Random.Range(0, spawnRate));
+        yield return new WaitForSeconds(Random.Range(0, _spawnRate));
         int c = Random.Range(0, asteroids.Count); //c: classification
         if (c < asteroids.Count && c >= 0)
         {
@@ -44,10 +41,11 @@ public class ManageAsteroids : MonoBehaviour
             asteroid.GetComponent<AsteroidMove>().manager = this;
         }
     }
+    
     private void NewSpawn(GameObject obj)
     {
-        Vector3 pos = Random.onUnitSphere * Random.Range(minSpawnRadius, maxSpawnRadius);
-        
+        Vector3 pos = Random.onUnitSphere * Random.Range(minSpawnRadius, maxSpawnRadius) ;
+
         //border spawn:
         obj.transform.position = pos + transform.position;
     }
@@ -56,10 +54,12 @@ public class ManageAsteroids : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float density = Mathf.Pow(Mathf.Max(maxSpawnRadius - minSpawnRadius, 1) * spawnDensity, 3)*Mathf.PI*(4/3);
+        if (spawnRate != 0) _spawnRate = (1 / (spawnRate + (density)));
         //scoreText.text = "Score: " + gameScore.ToString();
 
         float delta = Time.time - lastSpawn;
-        if (delta > spawnRate)
+        if (delta > _spawnRate)
         {
             lastSpawn = Time.time;
             for (int i = 0; i < spawnNum; i++)
