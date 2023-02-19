ecf1d98afe1a237c580c4232313951f48c7a6b59
Jacob Losco <jlosco2000@gmail.com>
2023-02-19 09:31:17 -0500

    added some effects and mats

--- AsteroidsThreeDee/Assets/Scripts/BulletMove.cs
+++ AsteroidsThreeDee/Assets/Scripts/BulletMove.cs
@@ -9,7 +9,7 @@ public class BulletMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-    
+        Destroy(this.gameObject, 5);
     }
 
     // Update is called once per frame
